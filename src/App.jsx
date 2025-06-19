07ac6d2acbd92b8a8eeed0970d74e7ce1922ea67
GandaUFF <qwert12121@hotmail.com>
2025-06-19 14:37:30 -0300

    feat: commit inicial e final do gerador de pdf

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Generatepdf from "./components/generatepdf"
+
+function App() {
+  
+  return (
+    <>
+      <h1>gerador pdf</h1>
+      <Generatepdf/>
+    </>
+  )
+}
+
+export default App
